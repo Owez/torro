fe4fde2551bd1cfe6cb67312a43f5d33489136f4
Owez <root@ogriffiths.com>
2020-09-05 22:08:40 +0100

    Added tagline & basic README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# torro
+
+An easy-to-use BitTorrent library
+
+## Status
+
+Heavily work-in-progress with no BitTorrent features currently fully supported. You may contribute [here](https://github.com/owez/torro) if you'd like.
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-/// Easy-to-use BitTorrent implementation
+/// An easy-to-use BitTorrent library
+/// 
+/// ## Status
+/// 
+/// Heavily work-in-progress with no BitTorrent features currently fully supported. You may contribute [here](https://github.com/owez/torro) if you'd like.
 /// 
 /// ## Specific modules
 /// 
