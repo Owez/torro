ac878cfcbb46f9fe90eb9da878de1a2c9a2bdb7c
Owez <root@ogriffiths.com>
2020-09-05 22:12:52 +0100

    Fixed tagline for docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
-/// An easy-to-use BitTorrent library
-/// 
-/// ## Status
-/// 
-/// Heavily work-in-progress with no BitTorrent features currently fully supported. You may contribute [here](https://github.com/owez/torro) if you'd like.
-/// 
-/// ## Specific modules
-/// 
-/// You may also choose to just use some modules inside of Torro for a specific
-/// purpose, here is a summary of available ones:
-/// 
-/// | Module name             | About                                                              |
-/// |-------------------------|--------------------------------------------------------------------|
-/// | [Parser](crate::parser) | `.torrent` parser core, used to interpret downloaded torrent files |
+//! An easy-to-use BitTorrent library
+//! 
+//! ## Status
+//! 
+//! Heavily work-in-progress with no BitTorrent features currently fully supported. You may contribute [here](https://github.com/owez/torro) if you'd like.
+//! 
+//! ## Specific modules
+//! 
+//! You may also choose to just use some modules inside of Torro for a specific
+//! purpose, here is a summary of available ones:
+//! 
+//! | Module name             | About                                                              |
+//! |-------------------------|--------------------------------------------------------------------|
+//! | [Parser](crate::parser) | `.torrent` parser core, used to interpret downloaded torrent files |
 
 pub mod parser;
