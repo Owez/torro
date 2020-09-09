618c5664d8cbbd3761aafeba5bef63d5ee053154
Owez <root@ogriffiths.com>
2020-09-09 20:02:35 +0100

    Updated README/tl docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,19 @@
-//! An easy-to-use BitTorrent library
-//!
-//! ## Status
-//!
-//! Heavily work-in-progress with no BitTorrent features currently fully supported. You may contribute [here](https://github.com/owez/torro) if you'd like.
-//!
+//! A **correct** and **easy-to-use** BitTorrent library
+//! 
+//! ## Objectives
+//! 
+//! - Easy-to-use library interface that assumes by default with customisability
+//! if needed
+//! - Extremely low dependency count (none ideally)
+//! - High amount of documentation, no data structures/functions implemented
+//! without a line of docstring
+//! - Correctness with the BitTorrent protocols
+//! 
+//! ## Development/Production Status
+//! 
+//! Heavily work-in-progress with no BitTorrent features currently fully supported.
+//! You may contribute [here](https://github.com/owez/torro) if you'd like.
+//! 
 //! ## Specific modules
 //!
 //! You may also choose to just use some modules inside of Torro for a specific
