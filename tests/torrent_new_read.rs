9289f9b8323f21fbc37e599c8b17b8514b2e6646
Owez <root@ogriffiths.com>
2020-09-15 19:40:05 +0100

    Added implementation test for Torrent::from_file

---
+++ tests/torrent_new_read.rs
@@ -0,0 +1,15 @@
+//! Ensures that [Torrent::from_file] can properly read a `.torrent` file
+
+use std::path::PathBuf;
+use torro::torrent::Torrent;
+
+const DATA_PATH_PREFIX: &str = "./tests/data/";
+
+/// Tests the proper reading for [Torrent::from_file], see module-level
+/// documentation for more information
+#[test]
+fn torrent_from_file() {
+    let file = PathBuf::from(format!("{}tiny.torrent", DATA_PATH_PREFIX));
+
+    Torrent::from_file(file).unwrap();
+}
